2e4185763b4521966774a804b08bc4653977d5a3
DW31 <ik012982i9@H012982.NET>
2017-05-05 09:53:13 +0200

    Ultimo modificado

---
+++ Tienda_java/model/ModeloRelleno.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ModeloRelleno {
+
+	public static final String NOMBRE_PRODUCTO_RELLENO = "";
+	public static final int STOCK = 0;
+	public static final Date FECHACADUCIDAD = convertirEnFecha("01/01/1960");
+	public static final Date FECHACOMPRA = null;
+	public static final int IDCOMPRA = 0;
+	public static final SimpleDateFormat sdfSoloFecha = new SimpleDateFormat("yyyy/MM/dd");
+
+	public static Date convertirEnFecha(String fecha) {
+
+		try {
+			return sdfSoloFecha.parse(fecha);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String deFechaAString(Date date) {
+		return sdfSoloFecha.format(date);
+
+	}
+}
---
+++ Tienda_java/model/Producto.java
@@ -0,0 +1,142 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Producto {
+	private int idProducto;
+	private int idCompra;
+	private String nombrePro;
+	private int stock;
+	private float precioUnidad;
+	private float precioTotal;
+	private Date fechaCaducidad;
+	private Date FechaCompra;
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private ArrayList<Producto> listCompra = new ArrayList<Producto>();
+	private ArrayList<Producto> listProductos = new ArrayList<Producto>();
+
+	// Constructora para mostrar la lista de compra
+	/*public Producto(String nombrePro, int stock, float precioUnidad) {
+		this.nombrePro = nombrePro;
+		this.stock = stock;
+		this.precioUnidad = precioUnidad;
+	}*/
+	
+	// Constructora que muestra todos los atributos de producto
+	/*public Producto(String nombrePro, float precioUnidad, Date fechaCaducidad, int stock) {
+		this.nombrePro = nombrePro;
+		this.stock = stock;
+		this.precioUnidad = precioUnidad;
+		this.fechaCaducidad = fechaCaducidad;
+	}*/
+	
+	//Constructora para tabla productos
+	public Producto(String nombrePro, float precioUnidad, Date fechaCaducidad, int stock) {
+		this.nombrePro = nombrePro;
+		this.stock = stock;
+		this.precioUnidad = precioUnidad;
+		this.fechaCaducidad = fechaCaducidad;
+	}
+	
+	public int getIdCompra() {
+		return idCompra;
+	}
+
+	public void setIdCompra(int idCompra) {
+		this.idCompra = idCompra;
+	}
+
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	public Date getFechaCompra() {
+		return FechaCompra;
+	}
+
+	public void setFechaCompra(Date fechaCompra) {
+		FechaCompra = fechaCompra;
+	}
+
+	//Constructora para tabla compras
+	public Producto (int idCompra, float precio, Date fechaCompra){
+		this.idCompra=idCompra;
+		this.precioTotal=precioTotal;
+		this.FechaCompra=FechaCompra;
+		
+	}
+	/*
+	 * public ArrayList <Producto> listProductoCompra(){ Producto listaCompra =
+	 * new Producto (nombrePro, stock, precioUnidad);
+	 * listCompra.add(listaCompra); return listCompra; } public ArrayList
+	 * <Producto> listaProductos(){ Producto listaProd = new Producto
+	 * (idProducto, nombrePro, stock, precioUnidad, fechaCaducidad);
+	 * listProductos.add(listaProd); return listProductos; }
+	 */
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getNombrePro() {
+		return nombrePro;
+	}
+
+	public void setNombrePro(String nombrePro) {
+		this.nombrePro = nombrePro;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public float getPrecioUnidad() {
+		return precioUnidad;
+	}
+
+	public void setPrecioUnidad(float precioUnidad) {
+		this.precioUnidad = precioUnidad;
+	}
+
+	public Date getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+
+	public void setFechaCaducidad(Date fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+
+	// Mostrar lista de compra
+	@Override
+	public String toString() {
+		return nombrePro + ":" + stock + ":" + precioUnidad + ":" + sdf.format(fechaCaducidad);
+	}
+
+	// Comprobar que no se inserten productos con mismo ID y nombre
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Producto) {
+			Producto prod = (Producto) obj;
+			if (idProducto == prod.getIdProducto() && nombrePro.equals(prod.getNombrePro())) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+}
